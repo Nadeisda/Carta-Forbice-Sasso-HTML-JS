a6cdc677d0e683327b736a0e7d120fe3c328b922
Nadeisda <nadeisdagiulia@gmail.com>
2024-06-11 15:38:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let punteggioUtente = 0;
+let punteggioPc = 0;
+
+function sceltaPc() {
+    const scelta = ['carta', 'forbice', 'sasso'];
+    const randomIndex = Math.floor(Math.random() * scelta.length);
+    return scelta[randomIndex];
+}
+
+function risultati(sceltaUtente, sceltaPc) {
+    if (sceltaUtente === sceltaPc) {
+        return "Pareggio!";
+    }
+    if (
+        (sceltaUtente === 'carta' && sceltaPc === 'sasso') ||
+        (sceltaUtente === 'forbice' && sceltaPc === 'carta') ||
+        (sceltaUtente === 'sasso' && sceltaPc === 'forbice')
+    ) {
+        punteggioUtente++;
+        return "Hai vinto!";
+    } else {
+        punteggioPc++;
+        return "Hai perso!";
+    }
+}
+
+function gioca(sceltaUtente) {
+    const sceltaDelPc = sceltaPc();
+    const risultato = risultati(sceltaUtente, sceltaDelPc);
+
+    document.getElementById('risultato').innerText = `Hai scelto ${sceltaUtente}. Il computer ha scelto ${sceltaDelPc}. ${risultato}`;
+    document.getElementById('punteggio-utente').innerText = punteggioUtente;
+    document.getElementById('punteggio-pc').innerText = punteggioPc;
+    document.getElementById('rigioca').style.display = 'block';
+}
+
+function rigioca() {
+    document.getElementById('risultato').innerText = '';
+    document.getElementById('rigioca').style.display = 'none';
+}
